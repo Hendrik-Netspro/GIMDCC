41406009e02d31cfafb19ef5dccbd13e30e5c8a2
hendrik <hendrik.hanking@icloud.com>
2026-06-14 14:43:59 +0200

    Add initial Rust project

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let projekt_name = "GIMDCC";
+    let voller_name = "Global Iron Man Desktop Control Center";
+    let version = "0.1.0";
+
+    println!("{projekt_name} startet...");
+    println!("{voller_name}");
+    println!("Version: {version}");
+    println!("Status: bereit fuer die erste Funktion");
+}
